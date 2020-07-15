43575b622afe6f3962cb7999041b67a7090d0315
Mohammad Ebrahim <faravaghi@users.noreply.github.com>
2020-07-15 18:58:47 +0430

    Create datetime.app.php

---
+++ src/messages/fa/datetime.app.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+	'Go to today' => 'برو به امروز',
+	'Clear selection' => 'پاک کردن انتخاب',
+	'Close the picker' => 'بستن پنجره',
+	'Select Month' => 'انتخاب ماه',
+	'Previous Month' => 'ماه قبل',
+	'Next Month' => 'ماه بعد',
+	'Select Year' => 'انتخاب سال',
+	'Previous Year' => 'سال قبل',
+	'Next Year' => 'سال بعد',
+	'Select Decade' => 'انتخاب دهه',
+	'Previous Decade' => 'دهه قبل',
+	'Next Decade' => 'دهه بعد',
+	'Previous Century' => 'قرن قبل',
+	'Next Century' => 'قرن بعد',
+	'Increment Hour' => 'افزایش ساعت',
+	'Pick Hour' => 'انتخاب ساعت',
+	'Decrement Hour' => 'کاهش ساعت',
+	'Increment Minute' => 'افزایش دقیقه',
+	'Pick Minute' => 'انتخاب دقیقه',
+	'Decrement Minute' => 'کاهش دقیقه',
+	'Increment Second' => 'افزایش ثانیه',
+	'Pick Second' => 'انتخاب ثانیه',
+	'Decrement Second' => 'کاهش ثانیه',
+	'Toggle Period' => 'تعویض دوره',
+	'Select Time' => 'انتخاب زمان'
+];
